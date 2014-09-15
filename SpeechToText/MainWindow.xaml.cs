fd3e581cd5e047da7f38075d51fabe099b820422
Philip Wheat <Phil@Philwheat.net>
2014-09-15 16:05:39 -0500

    Initial Commit

---
+++ SpeechToText/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Windows;
+using System.Diagnostics;
+
+namespace SpeechToText
+{
+    public partial class MainWindow : Window
+    {
+        SpeechRecognitionEngine speechRecognitionEngine = null;
+        List<Word> words = new List<Word>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                speechRecognitionEngine = createSpeechEngine("en-US");
+
+                speechRecognitionEngine.AudioLevelUpdated += new EventHandler<AudioLevelUpdatedEventArgs>(engine_AudioLevelUpdated);
+                speechRecognitionEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+
+                loadGrammarAndCommands();
+
+                speechRecognitionEngine.SetInputToDefaultAudioDevice();
+
+                speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Voice recognition failed");
+            }
+        }
+
+        private SpeechRecognitionEngine createSpeechEngine(string preferredCulture)
+        {
+            foreach (RecognizerInfo config in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                if (config.Culture.ToString() == preferredCulture)
+                {
+                    speechRecognitionEngine = new SpeechRecognitionEngine(config);
+                    break;
+                }
+            }
+
+            // if the desired culture is not found, then load default
+            if (speechRecognitionEngine == null)
+            {
+                speechRecognitionEngine = new SpeechRecognitionEngine(SpeechRecognitionEngine.InstalledRecognizers()[0]);
+            }
+
+            return speechRecognitionEngine;
+        }
+        private void loadGrammarAndCommands()
+        {
+            try
+            {
+                DictationGrammar defaultDictationGrammar = new DictationGrammar();
+                defaultDictationGrammar.Name = "default dictation";
+                defaultDictationGrammar.Enabled = true;
+
+                // Create the spelling dictation grammar.
+                DictationGrammar spellingDictationGrammar =
+                  new DictationGrammar("grammar:dictation#spelling");
+                spellingDictationGrammar.Name = "spelling dictation";
+                spellingDictationGrammar.Enabled = true;
+
+                Choices texts = new Choices();
+                string[] lines = File.ReadAllLines(Environment.CurrentDirectory + "\\example.txt");
+                foreach (string line in lines)
+                {
+                    // skip commentblocks and empty lines..
+                    if (line.StartsWith("--") || line == String.Empty) continue;
+
+                    // split the line
+                    var parts = line.Split(new char[] { '|' });
+
+                    // add commandItem to the list for later lookup or execution
+                    words.Add(new Word() { Text = parts[0], AttachedText = parts[1], IsShellCommand = (parts[2] == "true") });
+
+                    // add the text to the known choices of speechengine
+                    texts.Add(parts[0]);
+                }
+                Grammar wordsList = new Grammar(new GrammarBuilder(texts));
+                speechRecognitionEngine.LoadGrammar(defaultDictationGrammar);
+                speechRecognitionEngine.LoadGrammar(spellingDictationGrammar);
+                speechRecognitionEngine.LoadGrammar(wordsList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private string getKnownTextOrExecute(string command)
+        {
+            try
+            {
+                var cmd = words.Where(c => c.Text == command).First();
+
+                if (cmd.IsShellCommand)
+                {
+                    Process proc = new Process();
+                    proc.EnableRaisingEvents = false;
+                    proc.StartInfo.FileName = cmd.AttachedText;
+                    proc.Start();
+                    return "you just started : " + cmd.AttachedText;
+                }
+                else
+                {
+                    return cmd.AttachedText;
+                }
+            }
+            catch (Exception)
+            {
+                return command;
+            }
+        }
+
+        void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            txtSpoken.Text += "\r" + getKnownTextOrExecute(e.Result.Text);
+            scvText.ScrollToEnd();
+            SpeechRecognitionEngine eng = (SpeechRecognitionEngine)sender;
+            Metadata.Text += "  Confidence: " + e.Result.Confidence + "\r\n";
+            Metadata.Text += "Alternatives";
+            foreach (var alt in e.Result.Alternates)
+            {
+                Metadata.Text += " (" + alt.Text +") \r\n";
+            }
+            MetadataText.ScrollToEnd();
+        }
+
+        void engine_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            prgLevel.Value = e.AudioLevel;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // unhook events
+            speechRecognitionEngine.RecognizeAsyncStop();
+            // clean references
+            speechRecognitionEngine.Dispose();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ SpeechToText/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("SpeechToText")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpeechToText.Properties")]
+[assembly: AssemblyCopyright("(C) 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("2d4afebb-cde0-459c-a02a-31e1f3e42cc2")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
